d02fc6ecc457623432efd2c8e95042def0caef51
bryan <bemontoya@uninorte.edu.co>
2026-07-04 18:10:39 -0500

    feat: follow schema.sql

--- src/load.py
+++ src/load.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 load_dotenv()
 
@@ -9,14 +9,23 @@ def get_engine():
     url = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     return create_engine(url)
 
+def create_schema(engine):
+    with open("sql/schema.sql", "r") as f:
+        schema = f.read()
+    with engine.connect() as conn:
+        conn.execute(text(schema))
+        conn.commit()
+    print("Schema created successfully")
+
 def load(tables):
     engine = get_engine()
+    create_schema(engine)
     
     # orden importante: primero las que no tienen FK
-    tables['location'].to_sql('location', engine, if_exists='replace', index=False)
-    tables['date'].to_sql('dim_date', engine, if_exists='replace', index=False)
-    tables['provider'].to_sql('provider', engine, if_exists='replace', index=False)
-    tables['entity'].to_sql('entity', engine, if_exists='replace', index=False)
-    tables['contract'].to_sql('contract', engine, if_exists='replace', index=False)
+    tables['location'].to_sql('location', engine, if_exists='append', index=False)
+    tables['date'].to_sql('dim_date', engine, if_exists='append', index=False)
+    tables['provider'].to_sql('provider', engine, if_exists='append', index=False)
+    tables['entity'].to_sql('entity', engine, if_exists='append', index=False)
+    tables['contract'].to_sql('contract', engine, if_exists='append', index=False)
     
     print("Data uploaded successfully to PostgreSQL database.")
