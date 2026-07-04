ec00e3459763b3a8d7da0bc1a732d0aed1aae1f2
bryan <bemontoya@uninorte.edu.co>
2026-07-04 18:09:25 -0500

    feat: it's sure we get the columns in order we need

--- src/extract.py
+++ src/extract.py
@@ -7,13 +7,32 @@ import requests
 load_dotenv()
 
 api_secopii = os.getenv("API_SECOPII")
-csv_file = 'data/raw/secop2_data_2024-2025.csv'
+csv_file = 'data/raw/secop2-2426.csv'
 file_exists = os.path.exists(csv_file)
 
 def extract_data() -> pd.DataFrame:
     global file_exists
 
-    limit=50000
+    columns = [
+    # location
+    'departamento', 'ciudad',
+    
+    # entity
+    'codigo_entidad', 'nit_entidad', 'nombre_entidad', 'orden', 'sector', 'rama',
+
+    # contract
+    'id_contrato', 'estado_contrato', 'tipo_de_contrato', 'liquidaci_n', 'obligaci_n_ambiental',
+    'espostconflicto', 'valor_del_contrato', 'valor_pendiente_de_pago', 'valor_pagado',
+    
+    # provider
+    'codigo_proveedor', 'tipodocproveedor', 'documento_proveedor', 'proveedor_adjudicado', 'es_pyme',
+    
+    # dates
+    'fecha_de_firma', 'fecha_de_inicio_del_contrato', 'fecha_de_fin_del_contrato',
+    'fecha_inicio_liquidacion', 'fecha_fin_liquidacion'
+    ]
+
+    limit=5000
     df = pd.read_csv(csv_file) if file_exists else pd.DataFrame()
     offset = len(df)
         
@@ -21,7 +40,8 @@ def extract_data() -> pd.DataFrame:
     while True:
         try:
             response = requests.get(api_secopii, 
-                                    params={'$where': 'fecha_de_firma >= "2024-01-01" AND fecha_de_firma < "2026-01-01"',
+                                    params={'$select': ','.join(columns),
+                                            '$where': "fecha_de_firma >= '2024-01-01' and fecha_de_firma < '2026-01-01'",
                                             '$order': 'fecha_de_firma ASC',
                                             '$limit': limit, 
                                             '$offset': offset,
@@ -35,13 +55,14 @@ def extract_data() -> pd.DataFrame:
                 offset += limit
 
                 df_data = pd.DataFrame(data)
+                df_data = df_data[columns]  # Ensure the DataFrame has the correct column order
 
                 df_data.to_csv(csv_file, mode='a', header=not file_exists, index=False)
 
                 if not file_exists:
                     file_exists = True
 
-                print(f"Fetched {len(data)} records...")
+                print(f"Fetched {len(df_data)} records...")
 
             elif response.status_code == 503:
                 print("Service unavailable. Retrying...")
@@ -61,7 +82,14 @@ def extract_data() -> pd.DataFrame:
     
 
     df = pd.read_csv(csv_file) if file_exists else pd.DataFrame()
+
     print(f"Csv file with {len(df)} records...")
 
     return df
     
+
+if __name__ == "__main__":
+    df = extract_data()
+    print(df.head())
+    print(df.info())
+    print(df.describe())
